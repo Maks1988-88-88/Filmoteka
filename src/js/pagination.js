9d751e879f3540093c4b59e839a16554d898f636
Shuydo <shuydo12@gmail.com>
2021-06-10 02:30:29 +0300

    Закончена логика мобильной пагинации

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,73 +1,142 @@
-// console.log('pagination.shu');
-let pageNum = 1;
-let maxPage = 10;
-s1.classList.toggle("accent");
+// светлосерый под стрелкой - F7F7F7
+// accent - FF6B08 (background: rgba(255, 107, 8, 1))
 
-const reNum = () => {
+// тень над правой стрелкой в mobile:
+// filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+// transform: matrix(-1, 0, 0, 1, 0, 0);
+
+// 40x40 знакоместо под акцентированный номер страницы и фон стрелки
+// 5 - радиус скруглений этих фонов
+
+// font-family: Roboto;
+// font-style: normal;
+// font-weight: 500;
+// font-size: 12px;
+// line-height: 16px;/* identical to box height, or 133% */
+// display: flex;
+// align-items: center;
+// color: #000000;
+
+// m 20-40-19-[]-16-[]-17-40-16-[]-16-[]-20-40-20
+// t 197-40-16-[7]-16-.1.1.-16-[]-16-[]-17-40-16-[]-16-[]-16-.1.1.-16-[]-10-40-197
+
+// =>20-40-20-[]-16-[]-16-40-16-[]-16-[]-20-40-20
+
+let pageNum = 1;
+const maxPage = 10;
+// let visPage = 5;
+
+l1.classList.toggle("accent");
+
+const scrollNum = () => {
+  if (maxPage - pageNum === 1) {
+    s1.textContent = pageNum - 3;
+    s2.textContent = pageNum - 2;
+    s3.textContent = pageNum - 1;
+    s4.textContent = pageNum;
+    s5.textContent = pageNum + 1;
+    return;
+  }
+
+  if (pageNum === 1) {
+    s1.textContent = pageNum;
+    s2.textContent = pageNum + 1;
+    s3.textContent = pageNum + 2;
+    s4.textContent = pageNum + 3;
+    s5.textContent = pageNum + 4;
+    return;
+  }
+
+  if (pageNum === 2) {
+    s1.textContent = pageNum - 1;
+    s2.textContent = pageNum;
+    s3.textContent = pageNum + 1;
+    s4.textContent = pageNum + 2;
+    s5.textContent = pageNum + 3;
+    return;
+  }
+
+  if (maxPage === pageNum) return;
+
   s1.textContent = pageNum - 2;
   s2.textContent = pageNum - 1;
   s3.textContent = pageNum;
   s4.textContent = pageNum + 1;
   s5.textContent = pageNum + 2;
+};
 
-  s3.classList.add("accent");
+const accenting = () => {
+  // value.textContent = pageNum; // убрать после отладки
+
+  document.querySelector(".accent").classList.toggle("accent");
+
+  if (pageNum === 1) return l1.classList.add("accent");
+  if (pageNum === 2) return l2.classList.toggle("accent");
+  if (pageNum === maxPage - 1) return l4.classList.toggle("accent");
+  if (pageNum === maxPage) return l5.classList.add("accent");
+
+  l3.classList.add("accent");
+};
+
+const scrollNumIfNeed = () => {
+  // if (pageNum > 2 && pageNum + 2 <= maxPage) scrollNum();
+  // if (pageNum > 2 && pageNum + 2 <= maxPage) scrollNum();
+  // if (pageNum > 2)
+  scrollNum();
 };
 // ============= listeners ==================
 dec.addEventListener("click", () => {
-  if (pageNum - 1 > 0) pageNum -= 1;
-//   value.textContent = pageNum;
-console.log(pageNum);
+  if (pageNum - 1 <= 0) return;
+  pageNum -= 1;
 
-  if (pageNum === 1) {
-    s1.classList.add("accent");
-    s2.classList.remove("accent");
-    return;
-  }
-
-  if (pageNum === 2) {
-    s2.classList.toggle("accent");
-    s3.classList.toggle("accent");
-    return;
-  }
-
-  if (pageNum === maxPage-1) {
-    s4.classList.toggle("accent");
-    s5.classList.toggle("accent");
-    return;
-  }
-
-  if (pageNum === maxPage-2) {
-    s3.classList.toggle("accent");
-    s4.classList.toggle("accent");
-    return;
-  }
-
-  if(pageNum+2<maxPage)reNum();
-  
+  scrollNumIfNeed();
+  accenting();
 });
 
 inc.addEventListener("click", () => {
-  if(pageNum<maxPage)pageNum += 1;
-//   value.textContent = pageNum;
-console.log(pageNum);
+  if (pageNum >= maxPage) return;
+  pageNum += 1;
 
+  scrollNumIfNeed();
+  accenting();
+});
 
-  if (pageNum === 2) {
-    s1.classList.toggle("accent");
-    s2.classList.toggle("accent");
+l1.addEventListener("click", (click) => {
+  if (pageNum !== +click.currentTarget.innerText) {
+    pageNum = +click.currentTarget.innerText;
+    scrollNumIfNeed();
+    accenting();
   }
+});
 
-  if (pageNum === maxPage-1) {
-    s3.classList.toggle("accent");
-    s4.classList.toggle("accent");
+l2.addEventListener("click", (click) => {
+  if (pageNum !== +click.currentTarget.innerText) {
+    pageNum = +click.currentTarget.innerText;
+    scrollNumIfNeed();
+    accenting();
   }
+});
 
-  if (pageNum === maxPage) {
-    s4.classList.remove("accent");
-    s5.classList.add("accent");
+l3.addEventListener("click", (click) => {
+  if (pageNum !== +click.currentTarget.innerText) {
+    pageNum = +click.currentTarget.innerText;
+    scrollNumIfNeed();
+    accenting();
   }
+});
 
-  if (pageNum === 3) s2.classList.toggle("accent");
+l4.addEventListener("click", (click) => {
+  if (pageNum !== +click.currentTarget.innerText) {
+    pageNum = +click.currentTarget.innerText;
+    scrollNumIfNeed();
+    accenting();
+  }
+});
 
-  if ((pageNum > 2)&&(pageNum+2<=maxPage)) reNum();
+l5.addEventListener("click", (click) => {
+  if (pageNum !== +click.currentTarget.innerText) {
+    pageNum = +click.currentTarget.innerText;
+    scrollNumIfNeed();
+    accenting();
+  }
 });
